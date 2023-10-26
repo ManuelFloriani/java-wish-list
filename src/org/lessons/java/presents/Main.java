bd9f513302c9ae548244b5ccca5d4a097a48dfeb
Bufal8 <125837797+Bufal8@users.noreply.github.com>
2023-10-26 15:37:25 +0200

    Lettura e scrittura della lista regali su file di testo

--- src/org/lessons/java/presents/Main.java
+++ src/org/lessons/java/presents/Main.java
@@ -1,7 +1,13 @@
 package org.lessons.java.presents;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,5 +29,49 @@ public class Main {
         }
         System.out.println(gifts);
         scan.close();
+
+        FileWriter fileWriter = null;
+        try {
+
+            fileWriter = new FileWriter("./resources/listaRegali.txt", true);
+
+            for (int i = 0; i < gifts.size(); i++) {
+                fileWriter.write("Regalo");
+                fileWriter.write("\nDescrizione: " + gifts.get(i).getDescription());
+                fileWriter.write("\nDestinatario: " + gifts.get(i).getReceiver() + "\n \n");
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Impossibile scrivere sul file.");
+        }
+        finally {
+            try {
+                if (fileWriter != null) {
+                    System.out.println("Chiudo il file.");
+                    fileWriter.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        Scanner fileReader = null;
+        try {
+
+            fileReader = new Scanner(new File("./resources/data.txt"));
+
+            while (fileReader.hasNextLine()) {
+
+                String line = fileReader.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File non trovato.");
+        } finally {
+            if (fileReader != null) {
+                fileReader.close();
+            }
+        }
     }
 }
